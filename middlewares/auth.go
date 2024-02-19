744523f1aa481fcf2743fa757a121d50519a9b0c
Jose Manuel Escobedo Lopez <teco@Joses-MacBook-Air.local>
2024-02-18 23:06:41 -0600

    add comments to middleware

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -8,17 +8,20 @@ import (
 )
 
 func Auth(c *fiber.Ctx) error {
+	// Get token from cookie
 	access_token := c.Cookies("access_token")
 	if access_token == "" {
 		return utils.SendError(c, fmt.Errorf("no token, please signin"), fiber.StatusForbidden)
 	}
 
+	// Verify is a valid token
 	payload, err := utils.VerifyToken(access_token)
 	if err != nil {
 		c.ClearCookie("access_token")
 		return utils.SendError(c, fmt.Errorf("invalid token, please signin"), fiber.StatusUnauthorized)
 	}
 
+	// Send the user id to protected routes
 	c.Locals("userID", payload.USERID)
 
 	return c.Next()
